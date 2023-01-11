6ba08b171d0f3e1428bce52fbe9f4a5862339edd
IGWE RICKSON IKENNA <rickybeats24@gmail.com>
2023-01-11 03:53:27 +0000

    Update Stage2.py

--- Stage2.py
+++ Stage2.py
@@ -1,7 +1,7 @@
 import random
 
 
-class MoveRecord:
+class MovieRecord:
     """A class to represent a movie record"""
 
     def __init__(self, title, year, genre, release_date):
@@ -20,13 +20,13 @@ class MoveRecord:
             raise ValueError('Invalid argument type')
 
         # Generate a random ID for the move record
-        self._id = random.randint(1000000000, 9999999999)
+        self.id = random.randint(1000000000, 9999999999)
 
         # Assign attributes to instance variables
-        self._title = title
-        self._year = year
-        self._genre = genre
-        self._release_date = release_date
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.release_date = release_date
 
     # Method for Setting Title
     def set_title(self, title):
@@ -36,7 +36,7 @@ class MoveRecord:
         if not isinstance(title, str):
             raise ValueError("Title must be a string")
         # Set the title of the movie
-        self._title = title
+        self.title = title
 
     # Method for Setting Year
     def set_year(self, year):
@@ -58,26 +58,28 @@ class MoveRecord:
         if not isinstance(genre, str):
             raise ValueError("Genre must be a string")
         # Set genre of movie
-        self._genre = genre
+        self.genre = genre
 
     # Method for Setting Release Date
-    def set_release_date(self, release_date):
-        """Set release date of movie.
-        Args:release_date (datetime object): Release date of movie.
-        Raises: TypeError: If release date is not a datetime object."""
-
-    # # Check if release date is datetime object
-    #     if not isinstance(release_date, datetime.datetime):
-    #         raise TypeError("Release date must be a datetime object")
-
-        # Set release date of movie
-        self._release_date = release_date
+    def set_release_date(self, value):
+        # sets the movie release_date
+        # throws an exeption if the value is not empty or not of a string type and if the date format is not of the pattern MM/DD/YYYY
+        if not isinstance(value, str):
+            raise TypeError("Release_date must be a string")
+        if not value:
+            raise TypeError("Release_date cannot be empty")
+        valid: bool = re.search(
+            "^(0[1-9]|1[0-2])/(0[1-9]|[1-2][0-9]|3[0-1])/[0-9]{4}$", value)
+        if (not valid):
+            raise TypeError(
+                "Invalid release_date format, the date should be in MM/DD/YYYY format")
+        self.__release_date = value
 
     # Retrieve Title
     def get_title(self):
         """Retrieve the title of the movie"""
         try:
-            return self._title
+            return self.title
         except AttributeError:
             print("This movie does not have a title")
             return None
@@ -86,7 +88,7 @@ class MoveRecord:
     def get_year(self):
         """Retrieve the year of release for the movie"""
         try:
-            return self._year
+            return self.year
         except AttributeError:
             print("This movie does not have a year")
             return None
@@ -95,7 +97,7 @@ class MoveRecord:
     def get_genre(self):
         """Retrieve the genre of the movie"""
         try:
-            return self._genre
+            return self.genre
         except AttributeError:
             print("This movie does not have a genre")
             return None
@@ -104,7 +106,7 @@ class MoveRecord:
     def get_release_date(self):
         """Retrieve the release date of the movie"""
         try:
-            return self._release_date
+            return self.release_date
         except AttributeError:
             print("This movie does not have a release date")
             return None
@@ -113,98 +115,127 @@ class MoveRecord:
 class MovieList:
     """A class to store a collection of movie objects"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Constructor to create an object of the MovieList class"""
-        self.movies = {}  # Dictionary to store movie objects
+        self.__movieList = []
 
-    def add_movie(self, movie):
+    def add_movie(self, value):
         """A method to add a movie object to the collection"""
-        if not isinstance(movie, MoveRecord):  # Check if the argument is a Movie object
+        if not isinstance(value, MovieRecord):  # Check if the argument is a Movie object
             raise TypeError("The argument must be a MoveRecord object")
 
-        # Add the movie object to the dictionary with its Id as key and its title as value
-        self.movies[movie._id] = movie._title
-        # print(self.movies)
+        self.__movieList.append({
+            "id": value.id,
+            "genre": value.genre,
+            "release_date": value.release_date,
+            "title": value.title,
+            "year": value.year
+        })
 
-    def search_movie(self, title, genre, release_date):
-        """Searches through the movie collection and finds a movie by one or more
-        of the following movie attributes: title, genre or release date.
-        Parameters:
-        title (str): The title of the movie to search for.
-        genre (str): The genre of the movie to search for.
-        release_date (int): The release date of the movie to search for.
-        Returns:A list of movies that match the given criteria. """
+    @property
+    def movieInfo(self):
+        # retrieves the movie store data
+        return self.__movieList
 
-        # Initialize an empty list to store matching movies
-        matching_movies = []
+    @property
+    def movie_length(self):
+        # retrieves the total no of movies stored in the collection
+        return len(self.__movieList)
 
-        # Iterate through each movie in the collection
-        for movie in self.movies:
-            # Check if all criteria match
-            if (title == movie.title or title == None) and (genre == movie.genre or genre == None) and (release_date == movie.release_date or release_date == None):
-                # If all criteria match, add it to the list of matching movies
-                matching_movies.append(movie)
+    def search_movie_list(self, val, is_genre=False, is_title=False, is_release_date=False):
+        """
+        Search through the stored movie collection using the genre, release_date or title, 
+            - if the user doesn't include a movie attribute in the function params to search by, it defaults to search by title
+            - first format the input to all lowercase and the value searching for in order to annul the case-sensitive nature when searching
+        """
+        if not isinstance(val, str):
+            raise TypeError("Search value must be a string")
+        if not val:
+            raise TypeError("Search value cannot be empty")
+        val = val.lower()
+        if (not is_genre and not is_title and not is_release_date):
+            is_title = True
+        for movie in self.__movieList:
+            if is_title:
+                if movie['title'].lower() == val:
+                    return movie
+            if is_genre:
+                if movie['genre'].lower() == val:
+                    return movie
+            if is_release_date:
+                if movie['release_date'].lower() == val:
+                    return movie
+        raise Exception("Movie Data not found")
 
-            # Return the list of matching movies
-            return matching_movies
-
-    def remove_movie(self, title):
-        """Removes a movie from the collection based on its title.
-        Parameters:
-        title (str): The title of the movie to be removed from the collection.
-        Returns: True if successful, False otherwise."""
-
-        # Iterate through each movie in the collection
-        for i in range(len(self.movies)):
-
-            # Check if titles match
-            if self.movies[i].title == title:
-
-                # If titles match, remove it from the collection
-                del self.movies[i]
-
-                # Return True as removal was successful
-                return True
-
-        # Return False as removal was unsuccessful
-        return False
-
-    def get_total_movies(self, collection):
-        """ This method calculates and returns the total number of movies stored in the collection.
-        Parameters: 
-        collection (list): A list of movie objects
-        Returns: 
-        int: The total number of movies stored in the collection
-        Raises: TypeError: If the input is not a list """
-
-        # Check if the input is a list, if not raise a TypeError
-        if not isinstance(collection, list):
-            raise TypeError("Input must be a list")
-
-        # Initialize a counter to 0
-        total_movies = 0
-
-        # Iterate through each item in the collection and increment the counter by 1 for each movie object found.
-        for item in collection:
-            # Check if item is an instance of Movie class.
-            if isinstance(item, Movie):
-                total_movies += 1
-
-        return total_movies
+    def remove_movie(self, value):
+        """
+        Removes the movie that has the title provided in function params value
+        first format the input to all lowercase and the value searching for in order to anull the case-sensitive nature when searching
+        """
+        if not isinstance(value, str):
+            raise TypeError("Title must be a string")
+        if not value:
+            raise TypeError("Title cannot be empty")
+        value = value.lower()
+        for i in range(len(self.__movieList)):
+            if self.__movieList[i]['title'].lower() == value:
+                del self.__movieList[i]
+                return "Movie Data was Deleted Successfully"
+        raise Exception("Movie Data not found")
 
 
-a1 = MoveRecord('2Sugar', 2022, 'Afro', "1996")
-b1 = MoveRecord('PostgreSql', 2015, 'White', '1888')
+a1 = MovieRecord('2Sugar', 2022, 'Afro', "1996")
+b1 = MovieRecord('PostgreSql', 2015, 'White', '1888')
 
-# print(a1._title)
+# print(a1.title)
 # print(a1.get_release_date())
 # print(a1.get_title())
 # a1.set_title('yeah yeah')
 # print(a1.get_title())
 
-m2 = MovieList()
-# print(m2)
-m2.add_movie(a1)
-m2.add_movie(b1)
-# print(m2.add_movie('Faith'))
-print(m2.search_movie('Faith', 'Afro', 1995))
+# m2 = MovieList()
+# # print(m2)
+# m2.add_movie(a1)
+# m2.add_movie(b1)
+
+# print(m2.search_movie_list('PostgreSql'))
+
+movie = MovieRecord(title="Prison Break", year=2000,
+                    genre="Hiphop", release_date="13/02/1991")
+movie2 = MovieRecord(title="Prison Break", year=2000,
+                     genre="", release_date="08/02/1997")
+movie3 = MovieRecord(title="Blacklist", year=2018,
+                     genre="Comedy", release_date="12/02/2019")
+print(movie.id)
+print(movie.title)
+print(movie.year)
+movie.year = 2003
+movie.title = "Anti"
+print(movie.title)
+print(movie.year)
+movie.release_date = "12/02/2001"
+print(movie.release_date)
+print(movie.genre)
+print(movie.id)
+print(movie.title)
+print(movie.year)
+movie.year = 2022
+movie.genre = "Violence"
+movie.title = "Ascramony"
+movie.release_date = "09/23/2023"
+print(movie.release_date)
+print(movie.genre)
+print(movie.id)
+print(movie.title)
+print(movie.year)
+
+movieData = MovieList()
+movieData.add_movie(movie)
+movieData.add_movie(movie2)
+movieData.add_movie(movie3)
+print(movieData.movieInfo)
+print(movieData.movie_length)
+print(movieData.search_movie_list(val="Blacklist"))
+print(movieData.remove_movie(value="Prison Break"))
+print(movieData.movie_length)
+# print(movieData.remove_movie("Prison Break"))
